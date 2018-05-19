48258c3c11c4503d5a61eef95f994e8d0c707297
Saipranav Janyavula <saipranav.janyavula@frogforce503.org>
2018-05-19 13:38:22 -0400

    Bunch of changes

--- VirtualPlanner/src/virtualplanner/FXMLDocumentController.java
+++ VirtualPlanner/src/virtualplanner/FXMLDocumentController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -21,14 +22,27 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    private ComboBox comboBox = new ComboBox();
+    private ComboBox comboBox = new ComboBox<String>();
+    private TextField EventName = new TextField();
+      private TextField TimeOfEvent = new TextField();
+      private TextField PrepTime = new TextField();
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void speakBttnAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("123456789!");
     }
     
+    @FXML
+    private void submitBttnAction(ActionEvent event) {
+       String Event;
+        Event = EventName.getText();
+        String Time;
+        Time = TimeOfEvent.getText();
+        String Prep = PrepTime.getText();
+ 
+    }
+   
 
 
     
